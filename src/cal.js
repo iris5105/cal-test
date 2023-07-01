f5a625d89083a38bb0e100df08a7f8482f70be7f
iris5105 <iris510593@gmail.com>
2023-07-01 21:12:12 +0900

    header 데이터 tool box에 출력성공

--- src/cal.js
+++ src/cal.js
@@ -1,33 +1,40 @@
 import React, { useState, useEffect } from "react";
+import "rsuite/dist/rsuite.min.css";
 import { DateRangePicker } from "rsuite";
 import format from "date-fns/format";
-import isValid from "date-fns/isValid";
-import "./index.css";
 
 const CustomDateRangePicker = () => {
   const [selectedDate, setSelectedDate] = useState([new Date(), new Date()]);
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
+  const [PickedRange, setPickedRange] = useState([new Date(), new Date()]);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
-  const option = [
-    {
-      label: startDate + " ~ " + endDate,
-      value: selectedDate,
-    },
-  ];
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
   const renderValue = (value, formatStr) => {
     if (value[0] && value[1]) {
-      setStartDate(format(value[0], formatStr));
-      setEndDate(format(value[1], formatStr));
-      setSelectedDate([value[0], value[1]]);
-      console.log(selectedDate);
-      console.log(startDate);
-      console.log(endDate);
+      setStartDate(value[0]);
+      setEndDate(value[1]);
+      console.log("startDate: ", startDate);
+      console.log("endDate: ", endDate);
+      return `${format(value[0], formatStr)} - ${format(value[1], formatStr)}`;
     }
   };
+  useEffect(() => {
+    if (startDate && endDate) {
+      setPickedRange([startDate, endDate]);
+    }
+  }, [startDate, endDate]);
+  const ranges = [
+    {
+      label: `${format(PickedRange[0], "yyyy-MM-dd")} - ${format(
+        PickedRange[1],
+        "yyyy-MM-dd"
+      )}`,
+      value: PickedRange,
+    },
+  ];
 
   return (
     <DateRangePicker
@@ -35,7 +42,7 @@ const CustomDateRangePicker = () => {
       renderValue={renderValue}
       value={selectedDate}
       onChange={handleDateChange}
-      ranges={option}
+      ranges={ranges}
     />
   );
 };
